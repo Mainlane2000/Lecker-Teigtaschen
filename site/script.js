3b8ccf961266d21bd49e592491dd982cf0985b87
Alex <alex>
2025-02-20 19:44:17 +0900

    search bar complete

--- site/script.js
+++ site/script.js
@@ -6,13 +6,12 @@ function search(){
         suchfunktion(data);
         }
     };
-    xhttp.open("GET", "../test.json", true);
+    xhttp.open("GET", "../test2.json", true);
     xhttp.send();
 
     function suchfunktion(data) {
         //console.log(data);
         //var namearray = [];
-
         for(var i = 0; i < data.length; i++) {
             for(var i in data) {
                 if(namearray.indexOf(data[i].name) < 0) {
@@ -29,3 +28,77 @@ function search(){
 var namearray = [];
 search();
 console.log(namearray);
+
+function autocomplete(inp, arr) {
+    var currentFocus;
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        closeAllLists();
+        if(!val) {return false;}
+        currentFocus = -1;
+
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+
+        this.parentNode.appendChild(a);
+        console.log(a + "a1");
+        for(i = 0; i < arr.length; i++) {
+            if(arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                b = document.createElement("DIV");
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                console.log(b);
+                b.addEventListener("click", function(e) {
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+        }
+        console.log(b);
+        console.log(a + "a2");
+    });
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if(e.keyCode == 40) {
+            currentFocus++;
+            addActive(x);
+        } else if(e.keyCode == 38) {
+            currentFocus--;
+            addActive(x);
+        } else if(e.keyCode == 13) {
+            e.preventDefault();
+            if(currentFocus > -1) {
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+    function addActive(x) {
+        if (!x) return false;
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+    function closeAllLists(elmnt) {
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+}
+
+autocomplete(document.getElementById("myInput"), namearray);
