4c383ef7a975aaa4c85df38cdd7fa16ceda5cd28
Jago- <Jago-@192.168.2.163>
2025-03-18 18:57:04 +0100

    refactor map implementation to use Leaflet with improved structure and functionality

--- map/map_script.js
+++ map/map_script.js
@@ -1,31 +1,83 @@
+import L from 'leaflet';
+
+fuc = 0; // function use counter    0 = no function used yet
+
 function worldmap(){
-    var map = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,    
-    }).addTo(map);
-    var marker = L.marker([51.5, -0.09]).addTo(map);
-    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-    var circle = L.circle([51.508, -0.11], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 500
-    }).addTo(map);
-    circle.bindPopup("I am a circle.");
-    var polygon = L.polygon([
-        [51.509, -0.08],
-        [51.503, -0.06],
-        [51.51, -0.047]
-    ]).addTo(map);
-    polygon.bindPopup("I am a polygon.");
-    var popup = L.popup()
-    .setLatLng([51.5, -0.09])
-    .setContent("I am a standalone popup.")
-    .openOn(map);
-    map.on('click', function(e) {
-        popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-    });
+    if (fuc > 0) {
+        map.flyTo([0, 0], 0);
+
+    } else {
+        var map = L.map('map').setView([0, 0], 0);
+        startmap(map)
+    }
+    fuc++;          
 }
+
+function europe() {
+    if (fuc > 0) {
+        map.panTo([54.34, 20.88], 4);
+
+    } else {
+    var map = L.map('map').setView([54.34, 20.88], 4);
+    startmap(map)
+    }
+    fuc++;
+}
+function asia() {
+    if (fuc > 0) {
+        map.flyTo([48.63, 110.63], 3);
+
+    } else {
+    var map = L.map('map').setView([48.63, 110.63], 3);
+    startmap(map)
+    }
+    fuc++;
+}
+function africa() {
+    if (fuc > 0) {
+        map.flyTo([1.10, 25.90], 4);
+
+    } else {
+    var map = L.map('map').setView([1.10, 25.90], 4);
+    startmap(map)
+    }
+    fuc++;
+}
+function northamerica() {
+    if (fuc > 0) {
+        map.flyTo([53.80, -96.53], 3);
+
+    } else {
+    var map = L.map('map').setView([53.80, -96.53], 3);
+    startmap(map)
+    }
+    fuc++;
+}
+function southamerica() {
+    if (fuc > 0) {
+        map.flyTo([-22.72, -57.52], 4);
+
+    } else {
+    var map = L.map('map').setView([-22.72, -57.52], 4);
+    startmap(map)
+    }
+    fuc++;
+}
+function australia() {
+    if (fuc > 0) {
+        map.flyTo([-26.94, 153.58], 4);
+
+    } else {
+    var map = L.map('map').setView([-26.94, 153.58], 4);
+    startmap(map)
+    }
+    fuc++;
+}
+
+function startmap(map) {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 6,
+        attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+}
+
