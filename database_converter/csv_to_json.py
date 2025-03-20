eb50f8a801fc655516d2a74ad3345d73f00d2a4d
Jago- <Jago-@192.168.2.163>
2025-03-20 12:31:26 +0100

    feat: enhance CSV to JSON converter to support multiple CSV file conversion

--- database_converter/csv_to_json.py
+++ database_converter/csv_to_json.py
@@ -5,19 +5,32 @@ import pandas as pd
 import json
 
 # Defining the URL's
-url = './Files/Basedata/Kontinente.csv'
-output_url = './Files/Used_Data/json/Kontinente.json'
+url = './Files/Used_Data/csv/Dumplings.csv'
+url2 = './Files/Used_Data/csv/Countries_R_Lat_long.csv'
+output_url = './Files/Used_Data/json/Dumplings.json'
+
+#variable to choose multi csv conversion
+multi = False
+
+# Defining the function
 
 def csv_to_json(url):
+    if  (multi == True):
+        #Reading csv files
+        df1 = pd.read_csv(url)
+        df2 = pd.read_csv(url2)
 
-    # Reading the CSV File
-    df = pd.read_csv(url)
-    print(df.head())
+        #@ToDoo add code to convert multiple csv files to one json
 
-    # Creating a JSON File
-    js = df.to_json(output_url, orient='records', indent= 1)
+    else:    
+        # Reading the CSV File
+        df = pd.read_csv(url)
+        print(df.head())
 
-    print(f"JSON file has been created at {output_url}")
+        # Creating a JSON File
+        js = df.to_json(output_url, orient='records', indent= 1)
+
+        print(f"JSON file has been created at {output_url}")
 
 # Calling the function
 csv_to_json(url)
