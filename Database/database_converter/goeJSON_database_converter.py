46f5446d1aaac914e55426472ca43adb9136e7bc
Jago- <Jago-@192.168.2.163>
2025-03-28 16:12:25 +0100

    fix: update file paths and improve geoJSON creation logic in goeJSON_database_converter.py

--- Database/database_converter/goeJSON_database_converter.py
+++ Database/database_converter/goeJSON_database_converter.py
@@ -29,55 +29,52 @@ import pandas as pd
 import json
 
 # Defining the URL's
-url_Countries = './Files/Used_Data/csv/Countries_R_Lat_long.csv'
-url_Dumplings = './Files/Used_Data/csv/Dumplings.csv'
-url_geojson =   './Files/Used_Data/geoJSON/Dumplingpoints.geojson'
+url_Countries = 'Database/Map_Data/csv/Countries_R_Lat_long.csv'
+url_Dumplings = 'Database/Map_Data/csv/Teigtaschen_der_Welt.csv'
+url_geojson =   'Database/Map_Data/geoJSON/Dumplingpoints.geojson'
 
 def csv_to_geojson(url_Countries, url_Dumplings, url_geojson):
     # Reading the CSV Files
     df_Countries = pd.read_csv(url_Countries)
-    df_Dumplings = pd.read_csv(url_Dumplings, sep="\t")
-
-    #print(df_Countries.head())
-    #print(df_Dumplings.head())
+    df_Dumplings = pd.read_csv(url_Dumplings, sep=",")
 
     # Creating a geoJSON File
-
-    #definiton of the geoJSON
     geoJSON = {"type": "FeatureCollection", "features": []}
 
-    #getting the data from the CSV´s
-    for x in df_Countries['Index']:
+    # Iterating through the rows of df_Dumplings
+    for index, row in df_Dumplings.iterrows():
+        if row['Name_Teigtasche'] != 'na':
+            # Matching the country data using Landesindex
+            matching_country = df_Countries[df_Countries['Index'] == row['Landesindex']]
+            if not matching_country.empty:
+                country = matching_country.iloc[0]['Country']
+                region = matching_country.iloc[0]['Region']
+                lat = matching_country.iloc[0]['LAT']
+                long = matching_country.iloc[0]['LONG']
 
-        #getting the data from the CSV´s
-        country = df_Countries.loc[df_Countries['Index'] == x, 'Country'].values[0]
-        Region = df_Countries.loc[df_Countries['Index'] == x, 'Region'].values[0]
-        lat = df_Countries.loc[df_Countries['Index'] == x, 'LAT'].values[0]
-        long = df_Countries.loc[df_Countries['Index'] == x, 'LONG'].values[0]
-        dumpling = df_Dumplings.loc[df_Dumplings['Landesindex'] == x, 'Name_Teigtasche'].values[0]
-        Name_Landesprache = df_Dumplings.loc[df_Dumplings['Landesindex'] == x, 'Name_Landesprache'].values[0]
-        Zubereitungsart = df_Dumplings.loc[df_Dumplings['Landesindex'] == x, 'Zubereitungsart'].values[0]
-        Info = df_Dumplings.loc[df_Dumplings['Landesindex'] == x, 'Info'].values[0]
+                dumpling = row['Name_Teigtasche']
+                name_landesprache = row['Name_Landesprache']
+                zubereitungsart = row['Zubereitungsart']
+                info = row['Info']
 
+                # Creation of the geoJSON feature
+                feature = {
+                    "type": "Feature",
+                    "properties": {
+                        "Country": country,
+                        "Region": region,
+                        "Dumpling": dumpling,
+                        "Info": info,
+                        "popupContent": f"{dumpling} ist eine Teigtasche aus {country}. In der Sprache des Landes wird die Teigtasche {name_landesprache} genannt.\nDie Teigtasche wird {zubereitungsart}."
+                    },
+                    "geometry": {"type": "Point", "coordinates": [long, lat]}
+                }
 
-        #creation of the geoJSON
-        feature = {"type": "Feature",
-                   "properties": {
-                       "Country": country,
-                       "Region": Region,
-                       "Dumpling": dumpling,
-                       "Info": Info,
-                       "popupContent": f"{dumpling} ist eine Teigtasche aus {country}. In der Sprache des Landes wird die Teigtasche {Name_Landesprache} genannt.\nDie Teigtasche wird {Zubereitungsart}."
-                       },
-                   "geometry": {"type": "Point", "coordinates": [long, lat]}}
+                # Appending the geoJSON feature
+                geoJSON["features"].append(feature)
 
-        #appending the geoJSON
-        geoJSON["features"].append(feature)
-
-        #print(geoJSON)
-
-    #writing the geoJSON to file
+    # Writing the geoJSON to file
     with open(url_geojson, 'w') as f:
-        json.dump(geoJSON, f, indent= 1)
+        json.dump(geoJSON, f, indent=1)
   
 csv_to_geojson(url_Countries, url_Dumplings, url_geojson)
