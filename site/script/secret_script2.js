5534ac7244ab931ca877db88a0c75ce86561629c
Jago- <Jago-@192.168.2.163>
2025-03-28 18:49:15 +0100

    feat: add trivia game functionality and update HTML structure

--- site/script/secret_script2.js
+++ site/script/secret_script2.js
@@ -70,4 +70,47 @@ var fact_arr = [
         "id":12,
         "text":"Es gibt immer eine Teigtasche die man noch nie zuvor probiert hat, und es lohnt sich dem nachzukommen."
     }
-]
+];
+
+//json data
+var data;
+var json_url = "../../Database/TeigtaschenDerWelt.json";
+
+async function fetchData() {
+    try {
+        const response = await fetch(json_url);
+        data = await response.json();
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+fetchData();
+
+function trivia() {
+
+    var n = Math.floor(Math.random() * data.length);
+
+    if (data[n].Name_Teigtasche != "na" && data[n].Bilder_Link != "na") {
+        
+        var hTmL = document.getElementById("trivia_content");
+        var trivia_Game = "";
+        trivia_Game += "<h2>Trivia</h2>";
+        trivia_Game += "<figure><img src='" + data[n].Bilder_Link + "' alt='Teigtasche' width='300' height='300'><figcaption>'" + data[n].Credit_link + "'</figcaption></figure>";
+        trivia_Game += "<p>Wie heißt diese Teigtasche?</p>";
+        trivia_Game += "<button class='btn' id='btn1' onclick='checkAnswer(0)'>" + data[n].Name_Teigtasche + "</button>";
+        if (data[n + 10].Name_Teigtasche != "na") {
+            if (n + 10 >= data.length) {
+                trivia_Game += "<button class='btn' id='btn2' onclick='checkAnswer(1)'>" + data[n + 10].Name_Teigtasche + "</button>";
+            }
+            trivia_Game += "<button class='btn' id='btn2' onclick='checkAnswer(1)'>" + data[n - 10].Name_Teigtasche + "</button>";
+        }
+        hTmL.innerHTML = trivia_Game;
+    } else {
+        trivia();
+    }
+}
+
+function checkAnswer(answer) {
+    
+}
